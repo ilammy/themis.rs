0aaab72d1991cc2b93a60d9c10fd24c6e67eeb6a
ilammy <me@ilammy.net>
2019-10-06 11:57:31 +0300

    Random byte generator
    Implement the first and the easiest API of Soter: soter_rand(), ported
    to Rust. This gives an example of interfacing with BoringSSL and sets up
    some basic scaffolding for error handling.
    
    Here we do not implement C API for the call. I have experimented a bit
    with approaches and it seems that the best way would be to introduce
    a dedicated crate for all stuff related to interfacing with C. "soter"
    crate will contain only pure Rust API.
    
    Some notable design things:
    
      - Error does not implement PartialEq or Eq.
    
        I think it was a mistake to implement PartialEq for Error in
        "themis" crate because ultimately the errors are not comparable.
        You can compare error codes, but that does not mean that the
        "errors" themselves are comparable. Each has its own identity.
        And in the future I'd like to augment then with BoringSSL's
        error stack, for example, to simplify debugging.
    
      - Lack of error variants
    
        I believe that we should not return particularly detailed errors
        because that may incentivise developers to be 'smart' and ignore
        some of them because 'it should be safe'. So I have removed most
        of Soter's error distinctions and will prefer plain "Failure" as
        a catch-all error for most of the situations.
    
        In particular, out-of-memory errors are not distinguished because
        Rust currently does not have a preferable way to handle allocation
        failures (just aborts the process). Application developers are
        unlikely to do anything about them. Furthermore, it's probably
        a good practice to abort in security-related code as allocation
        errors are likely a sign of an incoming denial-of-service.
    
        Other errors are going to be collapsed into Failure as well,
        like SOTER_INVALID_SIGNATURE and SOTER_DATA_CORRUPT. For the user,
        both should be treated as "verification failed" and there is
        nothing that application can try to avoid them, other that using
        correct data.
    
        Some errors cannot be meaningfully recovered by the application,
        like failure to generate random data, and the interface is not
        safe to use if these errors are ignored, so we prefer to panic
        in these cases.
    
      - Raw BoringSSL errors
    
        soter-boringssl will just pass BoringSSL's error codes to Soter.
        We'll have to deal with error codes there. Well, most of the
        time we don't have to inspect them and can simply return Failure.

---
+++ src/soter/src/rand.rs
@@ -0,0 +1,57 @@
+// Copyright 2019 themis.rs maintainers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Generating random data.
+
+use crate::{Error, ErrorKind};
+
+/// Generates pseudo-random bytes.
+///
+/// This functions generates cryptographically strong pseudo-random bytes and fills
+/// the provided buffer with them.
+///
+/// # Panics
+///
+/// If the system does not have enough entropy to fill the entire buffer with random data,
+/// this function will panic. There is nothing that the application can do to avoid it,
+/// other than having being started after the system entropy pool had been properly seeded.
+///
+/// Note that some cryptographic backends might instead directly abort the process in this
+/// case, so you really should not try to ‘handle’ this failure.
+///
+/// # Example
+///
+/// ```
+/// # fn main() -> soter::Result<()> {
+/// use soter::rand;
+///
+/// let mut key = [0; 64];
+///
+/// rand::bytes(&mut key);
+/// # Ok(())
+/// # }
+/// ```
+pub fn bytes(buffer: &mut [u8]) {
+    if let Err(error) = boringssl::RAND_bytes(buffer) {
+        // Normally, BoringSSL will abort on failure, but double-tap just in case.
+        // If we are still alive then follow OpenSSL API for error reporting.
+        // One possible case is that the system does not have a CSPRNG available,
+        // which is equally fatal for the application.
+        let failure = match error {
+            -1 => Error::with_kind(ErrorKind::NotSupported),
+            _ => Error::with_kind(ErrorKind::Failure),
+        };
+        panic!(format!("failed to generate random bytes: {}", failure))
+    }
+}
