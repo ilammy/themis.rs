7caa0bb14efb1a80d4d741a58a11bcbc3b2fdc0f
ilammy <me@ilammy.net>
2020-08-01 22:13:45 +0300

    SHA-2 test suite
    Use some test vectors provided by NIST to verify that the
    implementation seems to indeed compute SHA-256 and SHA-512.
    There are some long tests thrown for completeness, you can
    enable those with "--features long_tests". They take a couple
    of seconds to run on modern hardware.
    
    Aside from test vectors, verify other important points in the
    implementation which are significant for the API.

--- src/soter/src/hash.rs
+++ src/soter/src/hash.rs
@@ -149,3 +149,163 @@ impl Hash {
         EVP_MD_CTX_size(&self.ctx)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::error::ErrorKind;
+
+    // We use quite long literals and hex_literal::hex! converts them into arrays,
+    // but Rust arrays have poor support in generics sometimes, so here's a helper
+    // macro hex! which is like hex_literal::hex!, but always outputs slices.
+    macro_rules! hex {
+        ($literal:expr) => {
+            &hex_literal::hex!($literal)[..]
+        };
+    }
+
+    // Test vectors provided by NIST et al.:
+    // https://csrc.nist.gov/projects/cryptographic-standards-and-guidelines/example-values
+    // https://www.di-mgt.com.au/sha_testvectors.html
+
+    mod sha256 {
+        use super::super::*;
+
+        #[test]
+        fn test_vectors() {
+            let test_vectors: &[(&[u8], &str)] = &[
+                (hex!("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"), ""),
+                (hex!("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"), "abc"),
+                (hex!("248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"), "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+                (hex!("cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1"), "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"),
+            ];
+            for (expected_output, input) in test_vectors {
+                let mut hash = Hash::new(Algorithm::SHA256);
+                hash.write(input);
+                assert_eq!(hash.get(), *expected_output);
+            }
+        }
+
+        #[test]
+        fn test_vectors_megabyte() {
+            let expected_output =
+                hex!("cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0");
+            let pattern = "a".repeat(1000);
+            let mut hash = Hash::new(Algorithm::SHA256);
+            for _ in 0..1000 {
+                hash.write(&pattern);
+            }
+            assert_eq!(hash.get(), expected_output);
+        }
+
+        #[test]
+        #[cfg_attr(not(feature = "long_tests"), ignore)]
+        fn test_vectors_gigabyte() {
+            let expected_output =
+                hex!("50e72a0e26442fe2552dc3938ac58658228c0cbfb1d2ca872ae435266fcd055e");
+            let pattern = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno";
+            let mut hash = Hash::new(Algorithm::SHA256);
+            for _ in 0..16777216 {
+                hash.write(&pattern);
+            }
+            assert_eq!(hash.get(), expected_output);
+        }
+    }
+
+    mod sha512 {
+        use super::super::*;
+
+        #[test]
+        fn test_vectors() {
+            let test_vectors: &[(&[u8], &str)] = &[
+                (hex!("cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"), ""),
+                (hex!("ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f"), "abc"),
+                (hex!("204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445"), "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"),
+                (hex!("8e959b75dae313da8cf4f72814fc143f8f7779c6eb9f7fa17299aeadb6889018501d289e4900f7e4331b99dec4b5433ac7d329eeb6dd26545e96e55b874be909"), "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"),
+            ];
+            for (expected_output, input) in test_vectors {
+                let mut hash = Hash::new(Algorithm::SHA512);
+                hash.write(input);
+                assert_eq!(hash.get(), *expected_output);
+            }
+        }
+
+        #[test]
+        fn test_vectors_megabyte() {
+            let expected_output = hex!("e718483d0ce769644e2e42c7bc15b4638e1f98b13b2044285632a803afa973ebde0ff244877ea60a4cb0432ce577c31beb009c5c2c49aa2e4eadb217ad8cc09b");
+            let pattern = "a".repeat(1000);
+            let mut hash = Hash::new(Algorithm::SHA512);
+            for _ in 0..1000 {
+                hash.write(&pattern);
+            }
+            assert_eq!(hash.get(), expected_output);
+        }
+
+        #[test]
+        #[cfg_attr(not(feature = "long_tests"), ignore)]
+        fn test_vectors_gigabyte() {
+            let expected_output = hex!("b47c933421ea2db149ad6e10fce6c7f93d0752380180ffd7f4629a712134831d77be6091b819ed352c2967a2e2d4fa5050723c9630691f1a05a7281dbe6c1086");
+            let pattern = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno";
+            let mut hash = Hash::new(Algorithm::SHA512);
+            for _ in 0..16777216 {
+                hash.write(&pattern);
+            }
+            assert_eq!(hash.get(), expected_output);
+        }
+    }
+
+    #[test]
+    fn output_sizes() {
+        assert_eq!(Hash::new(Algorithm::SHA256).output_size(), 256 / 8);
+        assert_eq!(Hash::new(Algorithm::SHA512).output_size(), 512 / 8);
+    }
+
+    #[test]
+    fn cannot_finalise_twice() {
+        let mut hash = Hash::new(Algorithm::SHA512);
+        let mut output = [0; 512 / 8];
+        assert!(hash.finalise(&mut output).is_ok());
+        assert!(hash.finalise(&mut output).is_err());
+    }
+
+    #[test]
+    #[should_panic(expected = "cannot write into finalised Hash")]
+    fn cannot_write_past_finalise() {
+        let mut hash = Hash::new(Algorithm::SHA256);
+        let mut output = [0; 256 / 8];
+        assert!(hash.finalise(&mut output).is_ok());
+        hash.write(b"abc"); // should panic
+    }
+
+    #[test]
+    #[should_panic(expected = "failed to finalise Hash")]
+    fn cannot_get_after_finalise() {
+        let mut hash = Hash::new(Algorithm::SHA256);
+        let mut output = [0; 256 / 8];
+        assert!(hash.finalise(&mut output).is_ok());
+        let _ = hash.get(); // should panic
+    }
+
+    #[test]
+    fn finalise_short() {
+        let mut hash = Hash::new(Algorithm::SHA256);
+        let mut output = [0; 128 / 8];
+        // You can't finalise if the buffer is too small.
+        let err = hash.finalise(&mut output).expect_err("not enough buffer");
+        assert_eq!(err.kind(), ErrorKind::BufferTooSmall(256 / 8));
+        // But you can after you reallocate.
+        let mut output = [0; 256 / 8];
+        assert!(hash.finalise(&mut output).is_ok());
+    }
+
+    #[test]
+    fn finalise_long() {
+        let mut hash = Hash::new(Algorithm::SHA256);
+        let mut output = [0xED; 512 / 8];
+        let result = hash.finalise(&mut output).expect("big buffer is fine");
+        // If the buffer is bigger than necessary, it's only partially filled.
+        let empty_sha256 = hex!("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+        assert_eq!(result, empty_sha256);
+        assert_eq!(&output[32..64], [0xED; 32]);
+    }
+}
